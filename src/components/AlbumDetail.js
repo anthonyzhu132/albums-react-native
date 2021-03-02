a30bce51ca081f333d01e2f0b586609c3b470d4e
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-03-01 22:42:05 -0800

    added styling and thumbnail image to each cardItem

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardItem from './CardItem';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+  const { thumbnailStyle, headerContentStyle } = styles;
+
   return (
     <Card>
       <CardItem>
-        <View></View>
-        <View style={styles.headerContentStyle}>
-          <Text>{props.album.title}</Text>
-          <Text>{props.album.artist}</Text>
+        <View>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }}/>
+        </View>
+        <View style={headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
         </View>
       </CardItem>
     </Card>
@@ -21,6 +26,10 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justiftyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
   }
 }
 
