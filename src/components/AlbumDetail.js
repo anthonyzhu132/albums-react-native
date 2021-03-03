c2fa9c2f5256ba5b8946c11589482e8d2f157ccb
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-03-02 18:37:28 -0800

    added Button component and styling

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardItem from './CardItem';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image, image } = album;
@@ -33,6 +34,10 @@ const AlbumDetail = ({ album }) => {
         style={imageStyle}
         source={{ uri: image }}
         />
+      </CardItem>
+
+      <CardItem>
+        <Button/>
       </CardItem>
     </Card>
   );
